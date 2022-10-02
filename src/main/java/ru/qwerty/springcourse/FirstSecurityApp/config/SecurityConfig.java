5285d16eecd720e95e6acf886037dc7758e34692
Soundmaker <soundmakersoz@gmail.com>
2022-10-02 18:57:04 +0300

    Custom page authorisation

--- src/main/java/ru/qwerty/springcourse/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/qwerty/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -1,9 +1,13 @@
 package ru.qwerty.springcourse.FirstSecurityApp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -14,9 +18,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public SecurityConfig(AuthProviderImpl authProvider) {
         this.authProvider = authProvider;
     }
+    // Конфигурируем Spring Security
+    // Конфигурируем авторизацию
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/auth/login","/error").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello",true)
+                .failureUrl("/auth/login?error");
+    }
 
     // Настраивает аутентификацию
     protected void configure(AuthenticationManagerBuilder auth) {
         auth.authenticationProvider(authProvider);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
