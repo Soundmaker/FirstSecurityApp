dabcf1d7d5cd68e12d381005d10b3f226fc0636b
Soundmaker <soundmakersoz@gmail.com>
2022-10-04 00:06:20 +0300

    CSRF

--- src/main/java/ru/qwerty/springcourse/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/qwerty/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -6,42 +6,44 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.qwerty.springcourse.FirstSecurityApp.services.PersonDetailsService;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final AuthProviderImpl authProvider;
+    private final PersonDetailsService personDetailsService;
 
-    @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
-    // Конфигурируем Spring Security
-    // Конфигурируем авторизацию
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/auth/login","/auth/registration","/error").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin().loginPage("/auth/login")
-                .loginProcessingUrl("/process_login")
-                .defaultSuccessUrl("/hello",true)
-                .failureUrl("/auth/login?error")
-                .and()
-                .logout().logoutUrl("/logout").logoutSuccessUrl("/auth/login");
+        // Конфигурируем Spring Security
+        // Конфигурируем авторизацию
+        http.authorizeRequests()
+            .antMatchers("/auth/login","/auth/registration","/error").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin().loginPage("/auth/login")
+            .loginProcessingUrl("/process_login")
+            .defaultSuccessUrl("/hello",true)
+            .failureUrl("/auth/login?error")
+            .and()
+            .logout().logoutUrl("/logout").logoutSuccessUrl("/auth/login");
     }
 
     // Настраивает аутентификацию
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider);
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService)
+                .passwordEncoder(getPasswordEncoder());
     }
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
