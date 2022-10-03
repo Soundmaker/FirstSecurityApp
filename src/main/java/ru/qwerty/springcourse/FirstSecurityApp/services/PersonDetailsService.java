def72cc30afb717047be80b4377e9cee0bd69b81
Soundmaker <soundmakersoz@gmail.com>
2022-10-03 16:02:49 +0300

    Registration with SpringSecurity

--- src/main/java/ru/qwerty/springcourse/FirstSecurityApp/services/PersonDetailsService.java
+++ src/main/java/ru/qwerty/springcourse/FirstSecurityApp/services/PersonDetailsService.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
+
 import org.springframework.stereotype.Service;
 import ru.qwerty.springcourse.FirstSecurityApp.models.Person;
 import ru.qwerty.springcourse.FirstSecurityApp.repositories.PeopleRepository;
---
+++ src/main/java/ru/qwerty/springcourse/FirstSecurityApp/services/RegistrationService.java
@@ -0,0 +1,22 @@
+package ru.qwerty.springcourse.FirstSecurityApp.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.qwerty.springcourse.FirstSecurityApp.models.Person;
+import ru.qwerty.springcourse.FirstSecurityApp.repositories.PeopleRepository;
+
+@Service
+public class RegistrationService {
+    private final PeopleRepository peopleRepository;
+
+    @Autowired
+    public RegistrationService(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+
+    @Transactional
+    public void register(Person person) {
+        peopleRepository.save(person);
+    }
+}
